1b5d6881a2b646e81ebf7c778883c2a01910c035
ZHR <1609354709@qq.com>
2024-06-21 20:46:06 +0800

    "我的"界面

--- lib/pages/individual.dart
+++ lib/pages/individual.dart
@@ -1,33 +1,60 @@
 import 'package:flutter/material.dart';
 
-
 class Individual extends StatelessWidget {
   const Individual({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-           
-          },
-          child: const Text('Go!'),
-        ),
+      body: Column(
+        children: [
+          // 用户信息部分
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                // 用户头像
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage:
+                      AssetImage('../../assets//images/user_avatar.png'),
+                ),
+                const SizedBox(width: 16),
+                // 用户昵称
+                const Text(
+                  '我的昵称',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          // 功能列表
+          Expanded(
+            child: ListView(
+              children: [
+                _buildListItem(Icons.person, '个性化', context),
+                _buildListItem(Icons.book, '账本管理', context),
+                _buildListItem(Icons.import_export, '账单导入导出', context),
+                _buildListItem(Icons.settings, '设置', context),
+                _buildListItem(Icons.help, '使用帮助', context),
+                _buildListItem(Icons.info, '关于', context),
+                _buildListItem(Icons.contact_mail, '联系我们', context),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
-}
 
-
-
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:SafeArea(child: Text('Page 2'),)
+  // 创建每一个列表项的小部件
+  Widget _buildListItem(IconData icon, String title, BuildContext context) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        print('点击了$title');
+      },
     );
   }
 }
