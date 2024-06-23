d061b8b2100e4bde92ebe43beb319226c1af5d43
ZHR <1609354709@qq.com>
2024-06-23 23:08:16 +0800

    终版

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lime/config/route_names.dart';
 import 'package:lime/route/routes.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
